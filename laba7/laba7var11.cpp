e3c5593fe37f4ea7e6abddfc6aa388ea8d857b53
GiC_on <MonsterJerkyMouseJack@gmail.com>
2025-11-27 15:27:20 +0300

    Lab is done (I hope so)

--- laba7/laba7var11.cpp
+++ laba7/laba7var11.cpp
@@ -1,27 +1,18 @@
 // Лабораторная работа 7 вариант 11
 // Чупов Максим ЭиН-2 1к.
 
-/*
-TO-DO LIST
- - check if FIOs aren't getting put to the char fio array
- - fix that program crashes after entering first student's FIO
- - make all other parts of the code 
-*/
-
 #include <iostream>
+#include <string>
 
 struct student
 {
-	char fio[40];
-	char adress[15];
+	std::string fio;
+	std::string adress;
 	int scores[3];
 };
 
-
 int main()
 {
-	std::setlocale(LC_ALL, "ru");
-
 	int studCount;
 
 	std::cout << "Enter students count: ";
@@ -29,17 +20,32 @@ int main()
 
 	student* studs = new student[studCount];
 	
+	// filling students' data
 	for(int i = 0; i < studCount; i++)
 	{
-		
-		std::cout << "\nEnter " << i+1 << " student's FIO: ";
-		std::cin >> studs[i].fio;
+		std::cin.ignore();
+		std::cout << "Enter " << i+1 << " student's FIO: ";
+		std::getline(std::cin, studs[i].fio);
 
 		std::cout << "Enter his/her adress: ";
-		std::cin >> studs[i].adress;
+		std::getline(std::cin, studs[i].adress);
 
-		std::cout << "Enter his/her scores for 3 exams: ";
-		std::cin >> studs[i].scores[0] >> studs[i].scores[1] >> studs[i].scores[2];
+		std::cout << "Enter his/her scores for 3 exams by space: ";
+		std::cin >> studs[i].scores[0] >> studs[i].scores[1]
+				 >> studs[i].scores[2];
+	}
+
+	std::cout << "\nStudents who live in Minsk city and have "
+			  << "average score not less than 4.5:\n";
+	
+	// finding the right students
+	for(int i = 0; i < studCount; i++)
+	{
+		if (studs[i].adress == "Minsk" && (studs[i].scores[0] +
+			studs[i].scores[1] + studs[i].scores[2]) / 3.0f >= 4.5f)
+		{
+			std::cout << studs[i].fio.substr(0, studs[i].fio.find(' ')) << "\n";
+		}
 	}
 
 	return 0;
